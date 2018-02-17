7c063c156e8b4b63c8ff1953179321ab32d3d8ec
sieunkim <sieunkr@gmail.com>
2018-02-17 23:30:32 +0900

    네이밍 변경

---
+++ cafe-autoconfig/src/test/java/springbasic/cafe/CafeApplicationTests.java
@@ -0,0 +1,16 @@
+package springbasic.cafe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CafeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
