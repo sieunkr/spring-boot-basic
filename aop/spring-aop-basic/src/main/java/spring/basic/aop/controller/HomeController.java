7431128d0a57ace48f0ddb75450b03671c7844b6
Eddy.Kim <sieunkr@gmail.com>
2018-04-30 15:10:48 +0900

    Update HomeController.java

--- aop/spring-aop-basic/src/main/java/spring/basic/aop/controller/HomeController.java
+++ aop/spring-aop-basic/src/main/java/spring/basic/aop/controller/HomeController.java
@@ -6,6 +6,7 @@ import spring.basic.aop.annotation.LogAspect;
 import spring.basic.aop.dto.Coffee;
 import spring.basic.aop.service.CoffeeService;
 
+import javax.websocket.server.PathParam;
 import java.util.Collection;
 
 @RestController
@@ -20,6 +21,13 @@ public class HomeController {
 
         return coffeeService.listCoffee();
     }
+
+    @GetMapping("/coffees/{name}")
+    public Coffee findCoffee(@PathVariable(name = "name") String name){
+
+        return coffeeService.findCoffee(name);
+    }
+
 
     @PostMapping("/coffees")
     public void createCoffee(){
